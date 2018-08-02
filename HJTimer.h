1913eb13aa054464003846ccf90329c06084d3ca
xiaobai1315 <jermy@JermydeMacBook-Pro.local>
2018-08-02 14:18:58 +0800

    commit

---
+++ HJTimer.h
@@ -0,0 +1,27 @@
+//
+//  HJTimer.h
+//
+//  Created by HJ
+//  Copyright © 2018年 All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HJTimer : NSObject
+
++ (NSString *)execTask:(void(^)(void))task
+           start:(NSTimeInterval)start
+        interval:(NSTimeInterval)interval
+         repeats:(BOOL)repeats
+           async:(BOOL)async;
+
++ (NSString *)execTask:(id)target
+              selector:(SEL)selector
+                 start:(NSTimeInterval)start
+              interval:(NSTimeInterval)interval
+               repeats:(BOOL)repeats
+                 async:(BOOL)async;
+
++ (void)cancelTask:(NSString *)name;
+
+@end
